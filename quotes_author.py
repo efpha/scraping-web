c72a1be739d52bc35412873f5f62bd59d9e40d90
Cepha <muthurikeffa254@gmail.com>
2023-09-26 14:21:14 +0300

    scraped

--- quotes_author.py
+++ quotes_author.py
@@ -6,3 +6,11 @@ headers = {
     "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36"
 }
 
+page = requests.get(url, headers=headers)
+soup = BeautifulSoup(page.content, 'html.parser')
+
+quotes = soup.findAll(class_ = "text")
+authors = soup.findAll(class_ = "author")
+
+for quote, author in zip(quotes, authors):
+    print(f'{quote.text + " by " + author.text}')
