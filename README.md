87a48716d256b39b3d15197826d5e155547693b5
efpha <muthurikeffa254@gmail.com>
2024-04-25 14:36:05 +0300

    Ignore scraped_quotes.csv and update .gitignore

---
+++ README.md
@@ -0,0 +1,5 @@
+# scraping-web
+
+Python web scraping
+
+Scraping quotes as well as their authors using BeautifulSoup
---
+++ quotes_author.py
@@ -0,0 +1,23 @@
+from bs4 import BeautifulSoup
+import requests
+import csv
+
+url = 'https://quotes.toscrape.com/'
+headers = {
+    "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36"
+}
+
+page = requests.get(url, headers=headers)
+soup = BeautifulSoup(page.content, 'html.parser')
+
+quotes = soup.findAll(class_ = "text")
+authors = soup.findAll(class_ = "author")
+
+f = open('scraped_quotes.csv', 'wt')
+writer = csv.writer(f)
+writer.writerow(["Quotes", "Authors"])
+
+for quote, author in zip(quotes, authors):
+    print(f'{quote.text + " , " + author.text}')
+    writer.writerow([quote.text, author.text])
+f.close()
